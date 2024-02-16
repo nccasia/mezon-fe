4fde7b88c36577bb72e2e96dc7c405f32bff3810
Nguyen Nam Phong <phongnn@devcamp.edu.vn>
2024-02-16 21:46:30 +0700

    host

--- apps/chat/src/app/app.tsx
+++ apps/chat/src/app/app.tsx
@@ -1,10 +1,6 @@
 import { MezonStoreProvider, initStore } from '@mezon/store';
 import { RouterProvider } from 'react-router-dom';
-import {
-  MezonContextProvider,
-  CreateMezonClientOptions,
-  useMezon,
-} from '@mezon/transport';
+import { MezonContextProvider, CreateMezonClientOptions, useMezon } from '@mezon/transport';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -17,64 +13,63 @@ import WebFont from 'webfontloader';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const GOOGLE_CLIENT_ID =
-  '1089303247801-qp0lhju8efratqkuk2murphealgdcseu.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '1089303247801-qp0lhju8efratqkuk2murphealgdcseu.apps.googleusercontent.com';
 
 const mezon: CreateMezonClientOptions = {
-  // host: 'dev-mezon.nccsoft.vn',
-  host: '127.0.0.1',
-  port: '7350',
-  key: 'defaultkey',
-  ssl: false,
+	host: 'dev-mezon.nccsoft.vn',
+	// host: '127.0.0.1',
+	port: '7350',
+	key: 'defaultkey',
+	ssl: false,
 };
 
 const theme = 'light';
 
 export function App() {
-  const mezon = useMezon();
-  const { store, persistor } = useMemo(() => {
-    return initStore(mezon, preloadedState);
-  }, [mezon])
-  if (!store) {
-    return <>loading...</>
-  }
-  return (
-    <MezonStoreProvider store={store} loading={null} persistor={persistor} >
-      <MezonUiProvider themeName={theme}>
-        <RouterProvider router={routes} />
-      </MezonUiProvider>
-    </MezonStoreProvider>
-  );
+	const mezon = useMezon();
+	const { store, persistor } = useMemo(() => {
+		return initStore(mezon, preloadedState);
+	}, [mezon]);
+	if (!store) {
+		return <>loading...</>;
+	}
+	return (
+		<MezonStoreProvider store={store} loading={null} persistor={persistor}>
+			<MezonUiProvider themeName={theme}>
+				<RouterProvider router={routes} />
+			</MezonUiProvider>
+		</MezonStoreProvider>
+	);
 }
 
 function AppWrapper() {
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ['Manrope'],
-      },
-    });
-  }, []);
+	useEffect(() => {
+		WebFont.load({
+			google: {
+				families: ['Manrope'],
+			},
+		});
+	}, []);
 
-  return (
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <MezonContextProvider mezon={mezon} connect={true}>
-        <App />
-        <ToastContainer
-          position='top-right'
-          autoClose={2200}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme='light'
-        />
-      </MezonContextProvider>
-    </GoogleOAuthProvider>
-  );
+	return (
+		<GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+			<MezonContextProvider mezon={mezon} connect={true}>
+				<App />
+				<ToastContainer
+					position="top-right"
+					autoClose={2200}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="light"
+				/>
+			</MezonContextProvider>
+		</GoogleOAuthProvider>
+	);
 }
 
 export default AppWrapper;
