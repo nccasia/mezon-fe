c2979d1ee995d3b66dd194e39f31453b669ae107
minhlucvanncc <145336454+minhlucvanncc@users.noreply.github.com>
2024-03-08 11:50:33 +0700

    implement build optimize

--- apps/chat/webpack.config.js
+++ apps/chat/webpack.config.js
@@ -2,13 +2,13 @@ const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
 
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const TerserPlugin = require("terser-webpack-plugin");
-
 
 // Nx plugins for webpack.
 module.exports = composePlugins(
-  withNx(),
+  withNx({
+    optimization: true,
+    namedChunks: true,
+  }),
   withReact({
     // Uncomment this line if you don't want to use SVGR
     // See: https://react-svgr.com/
@@ -18,17 +18,6 @@ module.exports = composePlugins(
     // Update the webpack config as needed here.
     // e.g. `config.plugins.push(new MyPlugin())`
     config.plugins.push(new NodePolyfillPlugin());
-    config.plugins.push(new BundleAnalyzerPlugin());
-
-    config.optimization = {
-      splitChunks: {
-        chunks: 'all',
-        minSize: 10000,
-        maxSize: 250000,
-      },
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    }
 
     config.resolve.fallback = { "fs": false };
     config.loader = {
