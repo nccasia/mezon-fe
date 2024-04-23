02a1e442091e1253dbbffc6f9ae62fd64052b1bd
devGangz <phongnn@devcamp.edu.vn>
2024-04-23 22:26:22 +0700

    update - 1

--- apps/chat/src/app/loaders/mainLoader.ts
+++ apps/chat/src/app/loaders/mainLoader.ts
@@ -1,11 +1,10 @@
-import { clansActions, emojiSuggestionActions, getStoreAsync, notificationActions } from '@mezon/store';
+import { clansActions, getStoreAsync, notificationActions } from '@mezon/store';
 import { gifsActions } from 'libs/store/src/lib/giftStickerEmojiPanel/gifs.slice';
 
 export const mainLoader = async () => {
 	const store = await getStoreAsync();
 	store.dispatch(clansActions.fetchClans());
 	store.dispatch(notificationActions.fetchListNotification());
-	store.dispatch(emojiSuggestionActions.fetchEmoji());
 	store.dispatch(gifsActions.fetchGifCategories());
 	store.dispatch(gifsActions.fetchGifCategoryFeatured());
 
