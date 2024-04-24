1bfc0df556126add33974a7d407780328be5e418
SonBeoDeveloper <hoaison1502@gmail.com>
2024-04-24 14:29:19 +0700

    son

--- apps/mobile/src/app/navigation/DrawerNavigator.tsx
+++ apps/mobile/src/app/navigation/DrawerNavigator.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { DrawerContentScrollView, DrawerItemList, createDrawerNavigator } from '@react-navigation/drawer';
 import { HEIGHT } from '../constants/config';
@@ -7,63 +7,63 @@ import Feather from 'react-native-vector-icons/Feather'
 import ServersScreen from '../screens/main/ClanScreen';
 import { darkColor } from '../constants/Colors';
 import CustomDrawerContent from '../components/ClanScreen/CustomDrawerContent';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = ({ navigation }) => {
+
     return (
-        <>
-            <BottomSheetModalProvider>
+        <BottomSheetModalProvider>
 
-                <Drawer.Navigator initialRouteName="Servers"
-                    drawerContent={props => <CustomDrawerContent {...props} />}
-                    screenOptions={{
-                        headerStyle: {
-                            backgroundColor: darkColor.Backgound_Primary,
-                        },
-                        headerTintColor: '#fff',
-                        headerTitleStyle: {
-                            fontWeight: 'bold',
-                        },
+            <Drawer.Navigator initialRouteName="Servers"
+                drawerContent={props => <CustomDrawerContent {...props} />}
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: darkColor.Backgound_Primary,
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
 
-                        drawerStyle: {
-                            backgroundColor: '#c6cbef',
-                            width: "90%",
-                            height: HEIGHT,
+                    drawerStyle: {
+                        backgroundColor: '#c6cbef',
+                        width: "90%",
+                        height: HEIGHT,
 
-                        },
-                        headerRight: () => (
-                            <View
-                                style={{
-                                    width: '100%',
-                                    height: 50,
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    flexDirection: 'row'
-                                }}
-                            >
-                                <View style={{ flexDirection: 'row', display: 'flex', alignItems: 'center', gap: 6 }}>
-                                    <Feather size={20} name="chevron-right" style={{ color: '#FFFFFF' }} />
-                                </View>
-                                <View style={{ flexDirection: 'row', marginRight: 10 }}>
-                                    <View style={{ borderRadius: 100, backgroundColor: '#323232', width: 35, height: 35, alignItems: 'center', justifyContent: 'center' }}>
-                                        <Feather size={20} name="search" style={{ color: '#FFFFFF' }} />
-                                    </View>
+                    },
+                    headerRight: () => (
 
-                                </View>
+                        <View
+                            style={{
+                                width: '100%',
+                                height: 50,
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                                flexDirection: 'row'
+                            }}
+                        >
+                            <View style={{ flexDirection: 'row', display: 'flex', alignItems: 'center', gap: 6 }}>
+                                <Feather size={20} name="chevron-right" style={{ color: '#FFFFFF' }} />
                             </View>
-                        ),
-                    }}>
+                            <View style={{ flexDirection: 'row', marginRight: 10 }}>
+                                <TouchableOpacity style={{ borderRadius: 100, backgroundColor: '#323232', width: 35, height: 35, alignItems: 'center', justifyContent: 'center' }}>
+                                    <Feather size={20} name="search" style={{ color: '#FFFFFF' }} />
+                                </TouchableOpacity>
 
-                    <Drawer.Screen name="Servers"
+                            </View>
+                        </View>
+                    ),
+                }}>
 
-                        component={ServersScreen} />
-                    <Drawer.Screen name="Messages" component={MessagesScreen} />
+                <Drawer.Screen name="Servers"
+
+                    component={ServersScreen} />
+                <Drawer.Screen name="Messages" component={MessagesScreen} />
 
 
-                </Drawer.Navigator>
-            </BottomSheetModalProvider>
-        </>
+            </Drawer.Navigator>
+        </BottomSheetModalProvider>
 
     );
 }
--- apps/mobile/src/app/screens/loading/SplashScreen.tsx
+++ apps/mobile/src/app/screens/loading/SplashScreen.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { HEIGHT, WIDTH } from '../../constants/config'
+import Images from 'apps/mobile/src/assets/Images'
 const SplashScreen = () => {
     return (
-        <View>
-            <Text>SplashScreen</Text>
+        <View style={{ width: WIDTH, height: HEIGHT, alignItems: 'center', justifyContent: 'center' }}>
+            <Image source={Images.ICON_GOOGLE} style={{ width: 100, height: 100 }} />
         </View>
     )
 }
