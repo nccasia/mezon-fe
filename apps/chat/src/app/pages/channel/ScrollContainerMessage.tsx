abfb9dcf882e8e17bd2f6017a9d50203c1ae6055
devGangz <phongnn@devcamp.edu.vn>
2024-04-28 08:10:52 +0700

    222

--- apps/chat/src/app/pages/channel/ScrollContainerMessage.tsx
+++ apps/chat/src/app/pages/channel/ScrollContainerMessage.tsx
@@ -8,36 +8,52 @@ export const ScrollContainerMessage = ({ children, scrollCta, channelId, mode, c
 	useEffect(() => {
 		loadMoreMessage();
 		setTriggerLoadMessage(true);
-	}, []);
+	}, [channelId]);
 
 	const outerDiv = useRef<HTMLDivElement | null>(null);
 	const innerDiv = useRef<HTMLDivElement | null>(null);
 	const prevInnerDivHeight = useRef<number | null>(null);
+
 	const outerDivHeight = outerDiv.current?.clientHeight;
 	const innerDivHeight = innerDiv.current?.clientHeight;
 	const outerDivScrollTop = outerDiv.current?.scrollTop;
 
-	const [outerDivHeightState, setOuterDivHeightState] = useState<number>(outerDivHeight ?? 0);
-	const [innerDivHeightState, setInnerDivHeightState] = useState<number>(innerDivHeight ?? 0);
+	const [outerDivHeightState, setOuterDivHeightState] = useState<number>(0);
+	const [innerDivHeightState, setInnerDivHeightState] = useState<number>(0);
+
+	useEffect(() => {
+		if (outerDivHeight && innerDivHeight) {
+			setOuterDivHeightState(outerDivHeight);
+			setInnerDivHeightState(innerDivHeight);
+		}
+	}, [outerDivHeight, innerDivHeight]);
+
 	const [outerDivScrollTopState, setOuterDivScrollTop] = useState<number>(outerDivScrollTop ?? 0);
 	const [messageToMap, setMessageToMap] = useState(messages);
 	const [triggerLoadMessage, setTriggerLoadMessage] = useState<boolean>(false);
 
+	console.log(outerDivHeight);
+	console.log(innerDivHeight);
+	console.log(outerDivScrollTopState);
+
 	useEffect(() => {
 		if (outerDivScrollTopState === 0) {
-			console.log(outerDivScrollTopState);
 			loadMoreMessage();
 			setTriggerLoadMessage(true);
 		}
-	}, [outerDivScrollTopState]); // Only listen to changes in outerDivScrollTopState
+	}, [outerDivScrollTopState, channelId]);
 
 	useEffect(() => {
-		console.log(messages);
 		if (triggerLoadMessage && messages.length > 0) {
 			setMessageToMap(messages);
 			setTriggerLoadMessage(false);
+			outerDiv.current?.scrollTo({
+				top: innerDivHeightState! - outerDivHeightState!,
+				left: 0,
+				behavior: 'smooth',
+			});
 		}
-	}, [triggerLoadMessage, messages]); // Update messageToMap whenever messages change
+	}, [triggerLoadMessage, messages, innerDivHeightState, outerDivHeightState]);
 
 	useEffect(() => {
 		if (outerDivHeightState && innerDivHeightState !== undefined) {
@@ -51,13 +67,18 @@ export const ScrollContainerMessage = ({ children, scrollCta, channelId, mode, c
 			prevInnerDivHeight.current = innerDivHeightState;
 		}
 	}, [outerDivHeightState, innerDivHeightState]);
+
 	const handleScroll = (e: any) => {
+		console.log('innerDivHeightState', innerDivHeightState!);
+		console.log('outerDivHeightState', outerDivHeightState!);
+		console.log(innerDivHeightState! - outerDivHeightState!);
 		setOuterDivScrollTop(e.target.scrollTop);
 	};
+
 	return (
 		<div className="relative h-full">
-			<div className="relative h-full overflow-scroll" ref={outerDiv} onScroll={handleScroll}>
-				<div className="relative transition-all duration-300" ref={innerDiv}>
+			<div className="relative h-full overflow-scroll border border-red-600" ref={outerDiv} onScroll={handleScroll}>
+				<div className="relative transition-all duration-300 border border-green-600" ref={innerDiv}>
 					{messageToMap.map((message, i) => (
 						<ChannelMessage
 							mode={mode}
