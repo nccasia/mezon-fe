ff42a122796a9db0244ea3fe6750b763c56c2df1
devGangz <phongnn@devcamp.edu.vn>
2024-06-08 12:46:57 +0700

    gg

--- libs/core/src/lib/app/hooks/useMessages.ts
+++ libs/core/src/lib/app/hooks/useMessages.ts
@@ -11,26 +11,14 @@ type MessageProps = {
 	messages: IMessageWithUser[];
 };
 
-<<<<<<< HEAD
 export const useMessages = ({ chatRef, channelId, hasMoreMessage, loadMoreMessage }: MessageProps) => {
-=======
-export const useMessages = ({ chatRef, channelId, hasMoreMessage, loadMoreMessage, messages }: MessageProps) => {
-	const dispatch = useDispatch();
->>>>>>> 91753c6addca428f403ea909a14fc23f6f697143
 	const [isFetching, setIsFetching] = useState(false);
 	const [currentChannelId, setCurrentChannelId] = useState(channelId);
 	const hasMoreMessage2 = useSelector(selectHasMoreMessage);
 	const remain = useSelector(selectQuantitiesMessageRemain);
-<<<<<<< HEAD
 	// useEffect(() => {
 	// 	const currentChatRef = chatRef.current;
 	// 	if (!currentChatRef || isFetching) return;
-=======
-	const [showLoadingMessage, setShowLoadingMessage] = useState<boolean>(false);
-	useEffect(() => {
-		const currentChatRef = chatRef.current;
-		if (!currentChatRef || isFetching) return;
->>>>>>> 91753c6addca428f403ea909a14fc23f6f697143
 
 	// 	if (channelId !== currentChannelId) {
 	// 		currentChatRef.scrollTop = currentChatRef.scrollHeight;
@@ -49,18 +37,7 @@ export const useMessages = ({ chatRef, channelId, hasMoreMessage, loadMoreMessag
 			const currentChatRef = chatRef.current;
 			if (!currentChatRef || isFetching) return;
 
-<<<<<<< HEAD
 			if (currentChatRef.scrollTop === 0) {
-=======
-			// If scrollTop is already 0, do not allow further scrolling up
-			if (currentChatRef.scrollTop === 0) {
-				if (hasMoreMessage2) {
-					return;
-				}
-				console.log(currentChatRef.scrollTop);
-				console.log('r');
-				setShowLoadingMessage(true);
->>>>>>> 91753c6addca428f403ea909a14fc23f6f697143
 				const previousHeight = currentChatRef.scrollHeight;
 				setIsFetching(true);
 				await loadMoreMessage();
@@ -77,9 +54,5 @@ export const useMessages = ({ chatRef, channelId, hasMoreMessage, loadMoreMessag
 		};
 	}, [loadMoreMessage, chatRef, isFetching, hasMoreMessage2]);
 
-<<<<<<< HEAD
 	return {isFetching};
-=======
-	return { isFetching, showLoadingMessage };
->>>>>>> 91753c6addca428f403ea909a14fc23f6f697143
 };
