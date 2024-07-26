d8f1f05dad577e5e532d783478efb0c87630509e
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-07-26 17:46:30 +0700

    updated

--- libs/components/src/lib/components/MessageWithUser/MessageLine.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageLine.tsx
@@ -1,6 +1,8 @@
-import { ChannelStreamMode } from 'mezon-js';
-import { memo, useMemo } from 'react';
-import { ChannelHashtag, EmojiMarkup, MarkdownContent, MentionUser, PlainText } from '../../components';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
+import { ChannelHashtag, EmojiMarkup, MarkdownContent, MentionUser, PlainText, PreClass } from '../../components';
 
 type MessageLineProps = {
 	mode?: number;
@@ -16,6 +18,70 @@ interface RenderContentProps {
 }
 
 // TODO: refactor component for message lines
+
+const MessageLine = ({ mode, content, showOnchannelLayout, onClickToMessage }: MessageLineProps) => {
+	return (
+		<div onClick={!showOnchannelLayout ? onClickToMessage : () => {}} className={`${showOnchannelLayout ? '' : 'cursor-pointer'}`}>
+			{/* <RenderContent data={content} mode={mode ?? ChannelStreamMode.STREAM_MODE_CHANNEL} showOnchannelLayout={showOnchannelLayout} /> */}
+			<FormattedMessageLine document={content.t}></FormattedMessageLine>
+		</div>
+	);
+};
+
+export default memo(MessageLine);
+
+const preprocessMarkdown = (markdown: string) => {
+	return markdown
+		.replace(/@(\w+)/g, '<span class="mention" data-tagname="@$1">@$1</span>')
+		.replace(/<#(\d+)>/g, '<span class="hashtag" data-tagname="<#$1>">#<$1></span>');
+};
+
+const FormattedMessageLine: React.FC<{ document: string }> = ({ document }) => {
+	const [content, setContent] = useState<React.ReactNode>(null);
+	const processedDocument = preprocessMarkdown(document);
+
+	const onClickLink = useCallback((url: string) => {
+		window.open(url, '_blank');
+	}, []);
+
+	useEffect(() => {
+		// Process Markdown content with plugins
+		setContent(
+			<Markdown
+				children={processedDocument}
+				remarkPlugins={[remarkGfm]}
+				rehypePlugins={[rehypeRaw]}
+				components={{
+					p: (props) => <p {...props} />,
+					span: (props) => {
+						const { className, ...rest } = props as { className?: string; 'data-tagname'?: string };
+						if (className === 'mention' && rest['data-tagname']) {
+							return <MentionUser tagName={rest['data-tagname']} />;
+						}
+						if (className === 'hashtag' && rest['data-tagname']) {
+							return <ChannelHashtag channelHastagId={rest['data-tagname']} />;
+						}
+					},
+					pre: PreClass,
+					a: (props) => (
+						<span
+							onClick={() => onClickLink(props.href ?? '')}
+							rel="noopener noreferrer"
+							style={{ color: 'rgb(59,130,246)', cursor: 'pointer' }}
+							className="tagLink"
+						>
+							{props.children}
+						</span>
+					),
+				}}
+			/>,
+		);
+	}, [document]);
+
+	// className="prose-code:text-sm inline prose-hr:my-0 prose-headings:my-0 prose-h1-2xl whitespace-pre-wrap prose  prose-blockquote:my-0 leading-[0]"
+	return <article className=" prose">{content}</article>;
+};
+
 const RenderContent = memo(({ data, mode, showOnchannelLayout }: RenderContentProps) => {
 	const { t, mentions = [], hashtags = [], emojis = [], links = [], markdowns = [] } = data;
 	const elements = [...mentions, ...hashtags, ...emojis, ...links, ...markdowns].sort((a, b) => a.startIndex - b.startIndex);
@@ -64,13 +130,3 @@ const RenderContent = memo(({ data, mode, showOnchannelLayout }: RenderContentPr
 	}, [elements, t, mode]);
 	return <div>{content}</div>;
 });
-
-const MessageLine = ({ mode, content, showOnchannelLayout, onClickToMessage }: MessageLineProps) => {
-	return (
-		<div onClick={!showOnchannelLayout ? onClickToMessage : () => {}} className={`${showOnchannelLayout ? '' : 'cursor-pointer'}`}>
-			<RenderContent data={content} mode={mode ?? ChannelStreamMode.STREAM_MODE_CHANNEL} showOnchannelLayout={showOnchannelLayout} />
-		</div>
-	);
-};
-
-export default memo(MessageLine);
