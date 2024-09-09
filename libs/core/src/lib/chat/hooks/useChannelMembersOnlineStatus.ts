ebd07a75345b19670009ffb248dd056ba2ca2149
zany <zany.le@mission.plus>
2024-09-09 11:45:49 +0700

    update logic member status mobile

--- libs/core/src/lib/chat/hooks/useChannelMembersOnlineStatus.ts
+++ libs/core/src/lib/chat/hooks/useChannelMembersOnlineStatus.ts
@@ -1,14 +1,19 @@
-import { selectMembersByChannelId } from '@mezon/store';
+import { ChannelsEntity, selectAllUserClans, selectChannelById, selectMemberIdsByChannelId, useAppSelector } from '@mezon/store';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { useAuth } from '../../auth/hooks/useAuth';
 
 export type UseChannelMembersOnlineStatusOptions = {
-	channelId?: string | null;
+	channel?: ChannelsEntity | null;
 };
 
-export function useChannelMembersOnlineStatus({ channelId }: UseChannelMembersOnlineStatusOptions = {}) {
-	const rawMembers = useSelector(selectMembersByChannelId(channelId as string));
+export function useChannelMembersOnlineStatus({ channel }: UseChannelMembersOnlineStatusOptions = {}) {
+	const parrentChannel = useAppSelector(selectChannelById(channel?.parrent_id as string));
+	const listMemberIds = useAppSelector((state) => selectMemberIdsByChannelId(channel?.id as string)(state));
+	const usersClan = useSelector(selectAllUserClans);
+
+	const rawMembers =
+		channel?.channel_private || parrentChannel?.channel_private ? usersClan.filter((item) => listMemberIds.includes(item.id)) : usersClan;
 	const { userId } = useAuth();
 
 	const [onlineMembers, offlineMembers] = useMemo(() => {
