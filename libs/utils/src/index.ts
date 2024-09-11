56c1a0eaafdb06e24bc003290f3f366f773f3fb7
Nguyen Ta Quyen <ta2k3quyen@gmail.com>
2024-09-11 07:16:18 +0700

    add api instance and intercepter with axios

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/api';
 export * from './lib/async';
 export * from './lib/bridge';
 export * from './lib/constant';
---
+++ libs/utils/src/lib/api/apiInstance.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+const baseURL = process.env.NX_BACKEND_FLOWISE_URL ?? '';
+const apiInstance = axios.create({
+	baseURL,
+	timeout: 5000,
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	withCredentials: true
+});
+apiInstance.interceptors.request.use((config) => {
+	// if (getCookie('accessToken') !== undefined) {
+	// 	config.headers.Authorization = 'Bearer ' + String(getCookie('accessToken'));
+	// }
+	return config;
+});
+apiInstance.interceptors.response.use(
+	(response) => {
+		return response.data;
+	},
+	async (error) => {
+		if (error.response?.status === 401) {
+			window.location.href = '/login';
+		}
+		return await Promise.reject(error);
+	}
+);
+export { apiInstance };
